74c22e7e9dc074626fb30d6a0506967b75546c93
madsouris <sourisservice@icloud.com>
2022-06-16 16:18:11 +0700

    added more buttons

--- src/components/Layout/MainContent.vue
+++ src/components/Layout/MainContent.vue
@@ -2,6 +2,23 @@
     <div class="row">
         <Product v-for="product in 8" :key="product" />
     </div>
+    <div class="row">
+        <div class="col">
+            <a
+                href="https://github.com/madsouris/storeFront"
+                target="_blank"
+                class="btn btn-primary mr-2"
+                >Repo 🍑
+            </a>
+            <a
+                href="https://vannrith.com"
+                target="_blank"
+                ref="nofollow"
+                class="btn btn-outline btn-primary"
+                >Author 🐭</a
+            >
+        </div>
+    </div>
 </template>
 
 <script>
